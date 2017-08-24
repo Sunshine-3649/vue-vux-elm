c67989adec8b70a54742f54892a768aeafffe8ac
WangYangYang <364933712@@qq.com>
2017-08-24 17:10:34 +0800

    添加eslint规则，添加分号

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 
 // 添加Fastclick移除移动端点击延迟
-const FastClick = require('fastclick')
-FastClick.attach(document.body)
+const FastClick = require('fastclick');
+FastClick.attach(document.body);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-import { AlertPlugin, ToastPlugin } from 'vux'
+import { AlertPlugin, ToastPlugin } from 'vux';
 
-Vue.use(AlertPlugin)
-Vue.use(ToastPlugin)
+Vue.use(AlertPlugin);
+Vue.use(ToastPlugin);
 
 /* eslint-disable no-new */
 new Vue({
@@ -21,4 +21,4 @@ new Vue({
   router,
   template: '<App/>',
   components: { App }
-})
+});
