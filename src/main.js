5f3a632c2cf8639a4d2edaa508f315c88c4a6023
WangYangYang <364933712@@qq.com>
2017-08-24 16:00:48 +0800

    安装依赖，添加到项目中

--- src/main.js
+++ src/main.js
@@ -4,7 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 添加Fastclick移除移动端点击延迟
+const FastClick = require('fastclick')
+FastClick.attach(document.body)
+
 Vue.config.productionTip = false
+
+import { AlertPlugin, ToastPlugin } from 'vux'
+
+Vue.use(AlertPlugin)
+Vue.use(ToastPlugin)
 
 /* eslint-disable no-new */
 new Vue({
